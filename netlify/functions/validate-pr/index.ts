629d4f11f8f0dbbfcceafa130de47a42cd8ed822
Maciej Pędzich <contact@maciejpedzi.ch>
2023-07-06 13:55:18 +0200

    Reintroduce AWS Lambda adapter

---
+++ netlify/functions/validate-pr/index.ts
@@ -0,0 +1,14 @@
+import {
+  createProbot,
+  createLambdaFunction
+} from '@probot/adapter-aws-lambda-serverless';
+
+import { appFn } from '../../../appFn';
+
+const privateKey = (process.env.PRIVATE_KEY as string).replace(/\\n/gm, '\n');
+
+const handler = createLambdaFunction(appFn, {
+  probot: createProbot({ overrides: { privateKey } })
+});
+
+export { handler };
