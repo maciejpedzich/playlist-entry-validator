80368e06477cfeb5208106d9b919bd13b0eb0cc1
Maciej Pędzich <contact@maciejpedzi.ch>
2023-07-06 11:42:13 +0200

    Add handling of non-multiline PRIVATE_KEY variable

--- netlify/functions/index.ts
+++ netlify/functions/index.ts
@@ -2,10 +2,13 @@ import {
   createLambdaFunction,
   createProbot
 } from '@probot/adapter-aws-lambda-serverless';
-import { appFn } from '../..';
+
+import { appFn } from '../../app';
+
+const privateKey = (process.env.PRIVATE_KEY as string).replace(/\\n/gm, '\n');
 
 const handler = createLambdaFunction(appFn, {
-  probot: createProbot()
+  probot: createProbot({ overrides: { privateKey } })
 });
 
 export { handler };
